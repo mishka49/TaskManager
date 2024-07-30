17f1190b68f143c2fc05b69b4094678d5566f8e9
mishka49 <andreym3ch@gmail.com>
2024-07-30 09:17:40 +0300

    init fastapi app

--- main.py
+++ main.py
@@ -1,15 +1,2 @@
-def print_info(message):
-    print(message)
-
-
-def update():
-    # This function update something
-    print("updated")
-
-
-def create():
-    print("create")
-
-
 if __name__ == "__main__":
-    print_info("Hellow world!!!")
+    pass
